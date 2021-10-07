2580bdd62f0b147881423956ec9cc0c4f555eba7
Lynne Munini <muninilynne65@gmail.com>
2021-10-07 05:59:21 -0400

    Changes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-my_money_machine = MoneyMachine()
-my_money_machine.report()
-my_coffee_maker = CoffeeMaker()
-my_coffee_maker.report()
+money_machine = MoneyMachine()
+money_machine.report()
+coffee_maker = CoffeeMaker()
+coffee_maker.report()
 menu = Menu()
 while True:
     options = menu.get_items()
@@ -12,13 +12,12 @@ while True:
     if choice == "off":
         break
     elif choice == "report":
-        my_money_machine.report()
-        my_coffee_maker.report()
+        money_machine.report()
+        coffee_maker.report()
     else:
         drink = menu.find_drink(choice)
-        if my_coffee_maker.is_resource_sufficient(drink):
-            if my_money_machine.make_payment(drink.cost):
-                my_coffee_maker.make_coffee(drink)
-
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
 
